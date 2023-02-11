16a93ed2d3b4e4ac979dcfa3169964db34442a49
Santiago Osorio G <santiagoo1042@gmail.com>
2023-02-11 17:09:17 -0500

    create tables

---
+++ database/migrations/2023_02_07_212828_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();           
+            $table->String('fullname');
+            $table->String('email')->unique();
+            $table->String('phone')->nullable();
+            $table->String('photo')->default('img/users/no-photo.png');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->String('password');
+
+
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
+
+            
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2024_02_07_210523_create_movies_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->String('name');
+            $table->text('description');
+            $table->enum('quality', ['cam', 'FullHd', '720p', '1080p', '4k']);
+            $table->String('image');
+            $table->date('release_year');
+            $table->integer('reproduction_number');
+            $table->String('language');
+            $table->String('video_link');
+            $table->String('download_link');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')
+                                            ->on('categories')
+                                            ->onUpdate('cascade')
+                                            ->onDelete('cascade');
+            
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
